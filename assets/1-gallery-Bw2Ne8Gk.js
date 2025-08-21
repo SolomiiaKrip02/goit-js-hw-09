4f590526dddcfc9af732c14d3488bee7c91f9db2
SolomiiaKrip02 <solomiya.kr08@gmail.com>
2025-08-21 12:32:46 +0000

    Deploying to gh-pages from @ SolomiiaKrip02/goit-js-hw-09@1cae9ff3c0c954bce814c0aa9af354584170171d 🚀

--- assets/1-gallery-Bw2Ne8Gk.js
+++
@@ -1,8 +0,0 @@
-import{S as e}from"./vendor-CgTBfC_f.js";document.addEventListener("DOMContentLoaded",()=>{const a=[{small:"https://picsum.photos/id/1015/250/150",large:"https://picsum.photos/id/1015/1000/600",description:"Image 1"},{small:"https://picsum.photos/id/1016/250/150",large:"https://picsum.photos/id/1016/1000/600",description:"Image 2"},{small:"https://picsum.photos/id/1018/250/150",large:"https://picsum.photos/id/1018/1000/600",description:"Image 3"}],s=document.querySelector(".gallery");if(!s)return;const i=a.map(t=>`
-    <li class="gallery-item">
-      <a class="gallery-link" href="${t.large}">
-        <img class="gallery-image" src="${t.small}" alt="${t.description}" />
-      </a>
-    </li>
-  `).join("");s.innerHTML=i,new e(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250})});
-//# sourceMappingURL=1-gallery-Bw2Ne8Gk.js.map
